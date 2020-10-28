6f6074f54600a5cd5f98434b41ec24115ddfa84b
epg323 <epg323@ucla.edu>
2020-10-27 17:45:15 -0700

    Updates

---
+++ precache-manifest.67d9722b5fd9a4c5e7e2cbd5336f519e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1ee9fb3ad3768f6613d2dbbd1b3c07",
+    "url": "/buyent/index.html"
+  },
+  {
+    "revision": "2a08ce73cc9a17b1ed78",
+    "url": "/buyent/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b649bfb6e75007b45692",
+    "url": "/buyent/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a08ce73cc9a17b1ed78",
+    "url": "/buyent/static/js/2.8c861c7b.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/buyent/static/js/2.8c861c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b649bfb6e75007b45692",
+    "url": "/buyent/static/js/main.9645452e.chunk.js"
+  },
+  {
+    "revision": "1d0ef008a89f335d0fbf",
+    "url": "/buyent/static/js/runtime-main.3bf3bf58.js"
+  }
+]);
